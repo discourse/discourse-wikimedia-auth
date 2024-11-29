baba768ca32d31411e55e897ecbd4d69cbcc67cc
David Taylor <david@taylorhq.com>
2024-11-29 13:39:09 +0000

    DEV: Convert to native class syntax

--- assets/javascripts/discourse/components/wiki-username.js
+++ assets/javascripts/discourse/components/wiki-username.js
@@ -1,26 +1,26 @@
 import Component from "@ember/component";
 import { schedule } from "@ember/runloop";
 import { service } from "@ember/service";
+import { classNameBindings, tagName } from "@ember-decorators/component";
 import $ from "jquery";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  siteSettings: service(),
-
-  classNameBindings: [":wiki-username"],
-  tagName: "h2",
+@classNameBindings(":wiki-username")
+@tagName("h2")
+export default class WikiUsername extends Component {
+  @service siteSettings;
 
   didInsertElement() {
-    this._super(...arguments);
+    super.didInsertElement(...arguments);
     schedule("afterRender", () => {
       const $el = $(this.element);
       $el.insertAfter(".full-name");
       $(".full-name").toggleClass("add-margin", Boolean(this.user.name));
     });
-  },
+  }
 
   @discourseComputed("user.wiki_username")
   wikiUserUrl(wikiUsername) {
     return `${this.siteSettings.wikimedia_auth_site}/wiki/User:${wikiUsername}`;
-  },
-});
+  }
+}
