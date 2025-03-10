034949cc48a657b7af92a6971ab774f787b6f7fa
David Taylor <david@taylorhq.com>
2025-03-10 10:30:09 +0000

    DEV: Upgrade modifyClass syntax to remove object-literal decorators (#29)
    Decorators on object literal properties are unsupported in most modern JS tooling, including ts/glint and Prettier 3.0. Using the new native-class-based modifyClass syntax means we can use decorators safely

--- assets/javascripts/discourse/initializers/wikimedia.js
+++ assets/javascripts/discourse/initializers/wikimedia.js
@@ -1,18 +1,18 @@
-import discourseComputed from "discourse/lib/decorators";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
   name: "wikimedia",
   initialize() {
     withPluginApi("0.8.23", (api) => {
-      api.modifyClass("controller:preferences/account", {
-        pluginId: "discourse-wikimedia-auth",
-
-        @discourseComputed
-        canUpdateAssociatedAccounts() {
-          return false;
-        },
-      });
+      api.modifyClass(
+        "controller:preferences/account",
+        (Superclass) =>
+          class extends Superclass {
+            get canUpdateAssociatedAccounts() {
+              return false;
+            }
+          }
+      );
     });
   },
 };
